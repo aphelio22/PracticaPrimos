87d2761dfa39d2f57e2cf8742354a2055eebbb4d
aphelio22 <jrgaln.m@gmail.com>
2023-09-19 10:48:12 +0200

    Ejercicio PracticaPrimos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -18,7 +18,6 @@ public class Main {
                 System.out.println("Introduce el número final:");
                 int numFinalIntervalo = sc.nextInt();
                 int contador = 0;
-                //h
 
                 System.out.println("Buscando primos...");
                 System.out.println("Números primos en el intervalo:");
