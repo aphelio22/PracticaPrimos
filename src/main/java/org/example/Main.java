c093c1f645cf0c778f6af608edf90211afbb1a5f
aphelio22 <jrgaln.m@gmail.com>
2023-09-19 10:33:07 +0200

    Ejercicio PracticaPrimos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -23,7 +23,6 @@ public class Main {
                     if (esPrimo(i)) {
                         contador++;
                         System.out.println(i);
-                        ///h
                     }
                 }
                 System.out.println("Se han encontrado " + contador + " números primos.");
